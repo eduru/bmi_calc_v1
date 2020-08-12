5f116330686571c5727ae15e18c2adfbd0a65a52
Eduardo F. Ruiz <eduru@me.com>
2020-08-12 09:52:47 +0900

    added more advices and a start node app

--- src/Components/Advices.js
+++ src/Components/Advices.js
@@ -1,10 +1,10 @@
 //writing the advices depending on your bmi
-const underweight = "You are underweight. Try to gain more weight by adding progressively 200 calories every day to your diet while."
+const underweight = "You are underweight. Try to gain more weight by adding progressively 200 calories every day to your diet while. Do some strength exercise meanwhile so you will increase your BMI in a healthy way"
 const normalWeight = "Your BMI is normal. Keep eating healthy and exercise regularly."
-const overWeight = "You have overweight. Reduce your calorie intake by 200 every day and exercise regularly."
-const obesityClass1 = "You have obesity Class 1. Speak with a doctor."
-const obesityClass2 = "You have obesity Class 2. Speak with a doctor."
-const morbidObesity = "You have Morbid Obesity. Speak with a doctor."
+const overWeight = "You have overweight. Reduce your calorie intake by 400 every day and exercise regularly. Intermittent Fasting could be your ally to achieve those goals. You can do it!!"
+const obesityClass1 = "You have obesity Class 1. Speak with a nutritionist and start from today to take care of your health. Is hard and it will take time. This is not a sprint but a marathon. You can do it!!!"
+const obesityClass2 = "You have obesity Class 2. Speak with a nutritionist and start from today to take care of your health. Is hard and it will take time. This is not a sprint but a marathon. You can do it!!!"
+const morbidObesity = "You have Morbid Obesity. Speak with a doctor and start from today to take care of your health. Is hard and it will take time. This is not a sprint but a marathon. You can do it!!!"
 
 const adviceBasedOnYour = (bmi) => {
     if (bmi <= 18.5) {
